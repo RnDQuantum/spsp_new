40679644d1b3841d5f6403634f45513c46a6073f
BimaBakti <bmbakti@gmail.com>
2025-10-13 18:39:03 +0700

    Add loading spinner and enhance chart tick formatting in GeneralReport component
    - Introduced a loading spinner to indicate data processing during event and aspect selection.
    - Updated chart tick formatting to append a percentage sign for better clarity.
    - Adjusted chart options to improve grid and offset settings for enhanced visual presentation.

--- resources/views/livewire/pages/general-report/statistic.blade.php
+++ resources/views/livewire/pages/general-report/statistic.blade.php
@@ -17,6 +17,17 @@
                 <option value="{{ $a['id'] }}">{{ strtoupper($a['category']) }} — {{ $a['name'] }}</option>
             @endforeach
         </select>
+
+        <div wire:loading wire:target="eventCode, aspectId" class="text-center">
+            <svg class="inline w-4 h-4 animate-spin" xmlns="http://www.w3.org/2000/svg" fill="none"
+                viewBox="0 0 24 24">
+                <circle class="opacity-25" cx="12" cy="12" r="10" stroke="currentColor" stroke-width="4">
+                </circle>
+                <path class="opacity-75" fill="currentColor"
+                    d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z">
+                </path>
+            </svg>
+        </div>
     </div>
 
     <!-- Judul Kurva -->
@@ -137,6 +148,10 @@
                             border: {
                                 display: false
                             },
+                            offset: true,
+                            grid: {
+                                offset: true
+                            }
                         },
                         y: {
                             min: 0,
@@ -145,7 +160,10 @@
                                 display: false
                             },
                             ticks: {
-                                stepSize: 1
+                                stepSize: 1,
+                                callback: function(value) {
+                                    return value + '%';
+                                }
                             }
                         }
                     }
