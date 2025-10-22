88bb7e6f58abafd4ee0ead57496e8f8824018730
BimaBakti <bmbakti@gmail.com>
2025-10-22 15:24:02 +0700

    add FinalReport livewire

---
+++ app/Livewire/Pages/IndividualReport/FinalReport.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Livewire\Pages\IndividualReport;
+
+use Livewire\Component;
+
+class FinalReport extends Component
+{
+    public function render()
+    {
+        return view('livewire.pages.individual-report.final-report');
+    }
+}
