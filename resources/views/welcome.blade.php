62ba7ff9ce2798f2f857d9a54e4ce17304088e49
Almendaris Shandy <shandypriyatama01@gmail.com>
2025-11-25 14:05:16 +0700

    same with the app.blade.php

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -6,6 +6,7 @@
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <title>STATIC PRIBADI SPIDER PLOT</title>
         @vite(['resources/css/app.css', 'resources/js/app.js'])
+        <link rel="icon" type="image/x-icon" href="{{ asset('images/thumb-qhrmi.png') }}">
     </head>
 
     <body class="bg-gray-50 min-h-screen flex items-center justify-center p-4">
