6fbbf705f3dcf75d5564ee268ab1700fc3fbc3af
BimaBakti <bmbakti@gmail.com>
2026-07-20 08:56:12 +0700

    style(sidebar): apply warm border token to divider in dark mode
    Replace the neutral-800 background with the warm-border token on
    the sidebar divider component to keep dark theme styling consistent

--- resources/views/components/sidebar/divider.blade.php
+++ resources/views/components/sidebar/divider.blade.php
@@ -2,6 +2,6 @@
     'margin' => 'my'
 ])
 
-<div class="h-px bg-neutral-200 dark:bg-neutral-800 {{ $margin }}-3 shrink-0"
+<div class="h-px bg-neutral-200 dark:bg-warm-border {{ $margin }}-3 shrink-0"
      x-show="!sidebarIsMini"
      x-transition></div>
